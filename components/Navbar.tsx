32cdbe5f80d3d639a38f16b567ff5f9a3454d87c
William Golden <wtgolden5@gmail.com>
2022-12-14 22:03:58 -0500

    remove styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { Flex } from './Flex.tsx';
 import { Text } from './Text.tsx';
 import { Title } from './Title.tsx';
-import './styles.css';
 
 type Props = {
   active: string;
@@ -17,24 +16,18 @@ export default function Header({ active }: Props) {
   return (
     <div class='bg-white w-full py-6 px-8 flex flex-col md:flex-row gap-4'>
       <div class='flex items-center flex-1 animate-fade-in-up'>
-        <Flex class='flex-row'>
+        <Flex class='flex-row ml-2'>
           <Title class='text-4xl font-semibold animate-fade-out-down hover:animate-bounce '>Will</Title>
           <Title class='text-4xl font-semibold animate-fade-out-down hover:animate-bounce px-1'>and</Title>
           <Title class='text-4xl font-semibold animate-fade-out-down hover:animate-bounce'>Kate</Title>
         </Flex>
       </div>
-      <ul class='flex items-end gap-6'>
+      <ul class='flex items-end gap-8 mr-6'>
         {menus.map((menu) => (
           <li>
-            <a href={menu.href}>
-              <Text
-                class={
-                  'text-gray-500 hover:text-green-100 py-1 border-gray-500 animate-fade-in-up text-semi-bold text-2xl link link-underline link-underline-black' +
-                  (menu.href === active ? ' font-bold border-b-2' : '')
-                }
-              >
-                {menu.name}
-              </Text>
+            <a class='group text-green-600 transition duration-300 ' href={menu.href}>
+              <Text class='text-gray-500 text-2xl animate-fade-in-up'>{menu.name}</Text>
+              <span class='block max-w-0 group-hover:max-w-full transition-all duration-500 h-[3px] bg-green-100'></span>
             </a>
           </li>
         ))}
