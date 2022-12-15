c68d33c1ea3e2d5b9ceaa525045d48c0e8dc1db0
William Golden <wtgolden5@gmail.com>
2022-12-14 20:59:55 -0500

    custom twind animations

--- twind.config.ts
+++ twind.config.ts
@@ -4,9 +4,6 @@ export default {
   selfURL: import.meta.url,
 
   theme: {
-    backgroundImage: {
-      "proposal": "url('/static/proposal.jpg')",
-    },
     colors: {
       gray: {
         100: "#DBE0E6",
@@ -37,6 +34,54 @@ export default {
     extend: {
       fontFamily: {
         sans: "Jost, sans-serif",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-down": {
+          "from": {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          "to": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          "from": {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          "to": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-down": "fade-out-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
       },
     },
   },
