2d375bf18ab35866420e3c1ab64089a6cf927872
William Golden <wtgolden5@gmail.com>
2022-12-23 21:13:28 -0500

    all pages added

--- islands/Home.tsx
+++ islands/Home.tsx
@@ -10,18 +10,35 @@ const Home = () => {
   const homeRef: MutableRef<null> = useRef(null);
   const aboutRef: MutableRef<null> = useRef(null);
   const registryRef = useRef(null);
-  const scrollToHome = () => homeRef && homeRef.current && homeRef.current.scrollIntoView({ behavior: 'smooth' });
+  const travelRef = useRef(null);
+  const scheduleRef = useRef(null);
+  const scrollToHome = () =>
+    homeRef &&
+    homeRef.current &&
+    homeRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'center' });
   const scrollToAbout = () =>
     aboutRef &&
     aboutRef.current &&
     aboutRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'center' });
   const scrollToRegistry = () =>
-    registryRef && registryRef.current && registryRef.current.scrollIntoView({ behavior: 'smooth' });
+    registryRef &&
+    registryRef.current &&
+    registryRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'center' });
+  const scrollToTravel = () =>
+    travelRef &&
+    travelRef.current &&
+    travelRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+  const scrollToSchedule = () =>
+    scheduleRef &&
+    scheduleRef.current &&
+    scheduleRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
 
   const menu = [
     { name: 'Home', scroller: scrollToHome },
     { name: 'About', scroller: scrollToAbout },
     { name: 'Registry', scroller: scrollToRegistry },
+    { name: 'Travel Info', scroller: scrollToTravel },
+    { name: 'Schedule', scroller: scrollToSchedule },
   ];
 
   return (
@@ -45,6 +62,20 @@ const Home = () => {
         <Flex class='flex flex-row items-center justify-center w-full h-full'>
           <div ref={registryRef}></div>
           <Text>REGISTRY HERE</Text>
+        </Flex>
+      </Flex>
+      <Flex class='flex flex-col h-screen w-screen'>
+        <Flex class='flex flex-row items-center justify-center w-full h-full'>
+          <div ref={travelRef}>
+            <Text>TRAVEL HERE</Text>
+          </div>
+        </Flex>
+      </Flex>
+      <Flex class='flex flex-col h-screen w-screen'>
+        <Flex class='flex flex-row items-center justify-center w-full h-full'>
+          <div ref={scheduleRef}>
+            <Text>SCHEDULE HERE</Text>
+          </div>
         </Flex>
       </Flex>
     </div>
