a14dae523e5d605594b4327b5f0903ac5d406fdd
William Golden <wtgolden5@gmail.com>
2023-03-01 19:01:19 -0500

    remove remult

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,9 +4,8 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/_middleware.ts";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $1 from "./routes/api/joke.ts";
+import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/About.tsx";
 import * as $$1 from "./islands/CardDisplay.tsx";
 import * as $$2 from "./islands/Home.tsx";
@@ -16,9 +15,8 @@ import * as $$4 from "./islands/RsvpReveal.tsx";
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/_middleware.ts": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/api/joke.ts": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
     "./islands/About.tsx": $$0,
